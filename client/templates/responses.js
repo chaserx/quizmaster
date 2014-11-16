c54df7907ee60b3f35831d764271993fdf79bc94
Chase Southard <chase.southard@gmail.com>
2014-11-16 11:25:26 -0500

    closer

--- client/templates/responses.js
+++ client/templates/responses.js
@@ -1,11 +1,31 @@
+Session.set("response", "?");
+Session.set("questionID", 1);
+
+$(document).ready(function() {
+  $('form:first *:input[type!=hidden]:first').focus();
+});
+
 Template.responses.helpers({
   response: function() {
     return Session.get("response");
+  },
+  questionID: function() {
+    return Session.get("questionID");
   }
 });
+
 Template.insertResponseForm.events({
-  'keydown input' : function(e){
+  'keydown input': function(e){
     console.log(String.fromCharCode(event.keyCode));
     Session.set("response", String.fromCharCode(event.keyCode));
+  }
+});
+
+Template.advanceQuestion.events({
+  'click button': function () {
+    Session.set("questionID", Session.get("questionID") + 1);
+    Session.set("response", "?");
+    $('#insertResponseForm')[0].reset();
+    $('form:first *:input[type!=hidden]:first').focus();
   }
 });
